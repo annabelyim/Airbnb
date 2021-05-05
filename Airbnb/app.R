90e1b8690365b7def03f837302e61d36a38df56e
Annabel Yim <annabelyim@gmail.com>
2021-05-06 00:26:01 +0900

    cleaning up

--- Airbnb/app.R
+++ Airbnb/app.R
@@ -1,21 +1,12 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+# Define UI for application 
 ui <- fluidPage(
    
    # Application title
    titlePanel("Airbnb"),
    
-   # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
         
@@ -24,28 +15,28 @@ ui <- fluidPage(
                             label = "Select City",
                             choices = c("Los Angeles", "New York City"),
                             selected = "Los Angeles"
-                     )
+                     ),
+        
+        selectInput(inputId = "neighbourhood",
+                    label = "Select Neighbourhood",
+                    choices = "",
+                    selected = ""
+        )
+      
       ),
       
-      # Show a plot of the generated distribution
+
       mainPanel(
-         plotOutput("plot")
+        
       )
    )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+# Define server 
+server <- function(input, output, session) {
+
+  }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
