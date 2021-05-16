fe6afe8ecb754690503aa7feb666e06ba68aa60c
vibhagogu <72591209+vibhagogu@users.noreply.github.com>
2021-05-16 18:31:42 -0400

    legend ammend

--- Airbnb/app.R
+++ Airbnb/app.R
@@ -3,12 +3,11 @@ library(tidyverse)
 library(maps)
 library(shinycssloaders)
 library(leaflet)
+library(GGally)
 
 # import data
 listings_la <- read_csv("~/Airbnb/UpdatedLAlistings.csv")
 listings_nyc <- read_csv("~/Airbnb/UpdatedNYClistings.csv") 
-listings_la$price <- as.numeric(gsub("[$,]", "", listings_la$price))
-listings_nyc$price <- as.numeric(gsub("[$,]", "", listings_nyc$price))
 
 # map variables
 counties <- map_data("county")
@@ -18,9 +17,6 @@ nyc_county <- subset(counties, region == "new york")
 # neighborhood variables 
 neighborhoods_la <- listings_la %>% distinct(neighborhood) %>% arrange(neighborhood)
 neighborhoods_nyc <- listings_nyc %>% distinct(neighborhood) %>% arrange(neighborhood)
-neighborhoods_all <- neighborhoods_la %>% 
-  rbind(neighborhoods_nyc)
-
 
 # Define UI for application 
 ui <- fluidPage(
@@ -28,9 +24,11 @@ ui <- fluidPage(
   # Application title
   titlePanel("Airbnb"),
   
+  # Filtering Panel 
   sidebarLayout(
     sidebarPanel(
       
+      # selecting city and neighborhoods 
       titlePanel("Airbnb Filters"),
       radioButtons(inputId = "city",
                    label = "Select City",
@@ -44,58 +42,64 @@ ui <- fluidPage(
                   selected = "", 
                   multiple = TRUE
       ),
-      uiOutput("slider_price"),
-      uiOutput("slider_reviews"),
+      
+      # sliders
+      sliderInput(inputId = "price_range", 
+                  label = "Average Price",
+                  min = 0, max = 500,
+                  value = c(50,150)),
+      sliderInput(inputId = "num_reviews", 
+                  label = "Number of Reviews",
+                  min = 0, max = 200,
+                  value = 10),
+      sliderInput(inputId = "num_guests", 
+                  label = "Number of Guests Allowed",
+                  min = 0, max = 16,
+                  value = 2),
       
       # check boxes
-      checkboxInput(inputId = "entire house",
-                    label = "Entire house",
-                    value = FALSE, 
-                    width = '100%'
-      ),
+      checkboxGroupInput(inputId = "room_types",
+                         label = "Accomodation Type",
+                         choices = c("Private room", "Shared room", "Hotel room", 
+                                     "Entire home/apt"),
+                         selected = c("Private room", "Shared room", "Hotel room", 
+                                      "Entire home/apt"),
+                         width = '100%'
+      )
       
-      checkboxInput(inputId = "washing machine",
-                    label = "Has a washer & dryer",
-                    value = FALSE, 
-                    width = '100%'
-      ),
-      
-      checkboxInput(inputId = "cooking utilities",
-                    label = "Home cooking utilities",
-                    value = FALSE, 
-                    width = '100%'
-      ),
-      
-      checkboxInput(inputId = "parking",
-                    label = "Free parking",
-                    value = FALSE, 
-                    width = '100%'
-      ),
     ),
     
     
     mainPanel(
       withSpinner(plotOutput(outputId = "map")),
       plotOutput(outputId = "avgprice_graph"),
-      plotOutput(outputId = "map2")
+      plotOutput(outputId= "avgratings_graph"),
+      leafletOutput(("mymap"))
     )
   )
 )
 
+
 # Define server 
 server <- function(input, output, session) {
   
-  # city selected
-  selected_city <- reactive({
+  # filters for all attributes selected
+  selected_attributes <- reactive({
     req(input$city)
+    df 
     if (input$city == "New York City") {
-      listings_nyc
+      df <- listings_nyc
     } else {
-      listings_la
+      df <- listings_la
     }
+    df %>% 
+      filter(price <= input$price_range[2] & price >= input$price_range[1],
+             number_of_reviews >= input$num_reviews[1],
+             accommodates >= input$num_guests[1],
+             room_type %in% input$room_types)
   })
   
-  # neighborhoods to dynamically changed based on city selected
+  # neighborhoods to dynamically changed based on city selected for map 
   selected_city_neighborhoods <- reactive({
     req(input$city)
     if (input$city == "New York City") {
@@ -112,18 +116,6 @@ server <- function(input, output, session) {
                       choices = selected_city_neighborhoods())
   })
   
-  output$slider_price <- renderUI({
-    sliderInput("avgprice", "Average Price",
-                min = 0, max = 500,
-                value = c(50,150))
-  })
-  
-  output$slider_reviews <- renderUI({
-    sliderInput("reviews", "Number of Reviews",
-                min = 0, max = 200,
-                value = 10)
-  })
-  
   selected_neighborhoods <- reactive({
     req(input$city)
     req(input$neighborhood)
@@ -134,11 +126,15 @@ server <- function(input, output, session) {
     }
   })
   
-  selected_city_prices <- reactive({
-    prices <- 2 * sd(selected_city()$price)
-    listings_prices <- selected_city() %>% 
-      filter(price <= prices)
-    listings_prices
+  selected_neighborhood_avg_ratings <- reactive({
+    req(input$city)
+    req(input$neighborhood)
+    avg_ratings <- selected_neighborhoods() %>% 
+      group_by(neighborhood) %>%
+      summarize(avg_rating = round(mean(review_scores_rating/10, na.rm = TRUE), 2), 
+                avg_cleanliness = round(mean(review_scores_cleanliness, na.rm = TRUE), 2),
+                avg_communication = round(mean(review_scores_communication, na.rm = TRUE), 2)) 
+    avg_ratings
   })
   
   # map 
@@ -151,8 +147,6 @@ server <- function(input, output, session) {
         theme_void() +
         guides(alpha = FALSE) 
       
-      
-      
     } else {
       ggplot() +
         geom_polygon(data = ca_county, aes(x = long, y = lat, group = group), color = "white", fill = "gray") +
@@ -164,68 +158,58 @@ server <- function(input, output, session) {
     
   })
   
-  #map2
-  # Create our colors with a categorical color function
-  bins <- c(0,20,40,60, 80, 100)
-  
-  output$map2 <- renderLeaflet({
-    if (input$city == "New York City") {
-      pal <- colorBin(c("#d7191c", "#fdae61", "#ffffbf", "#a6d96a", "#1a9641"), domain = listings_nyc$review_scores_rating, bins = bins)
-      map2 <- leaflet(listings_nyc) %>%
-        addProviderTiles("CartoDB.Positron", options = providerTileOptions(noWrap = TRUE)) %>%
-        addCircleMarkers(
-          lng = ~longitude,
-          lat = ~latitude,
-          radius=~price,
-          stroke=FALSE,
-          fillColor = ~pal(review_scores_rating),
-          color = "white",
-          fillOpacity=0.5,
-          labelOptions = labelOptions(noHide = FALSE),
-          popup = paste0( '<p><strong>', listings_nyc$name, '</strong> <p/>', 
-                          "<strong> Room Type: </strong>", 
-                          listings_nyc$room_type, '<br/>',
-                          "<strong> Price Per Night </strong>", 
-                          listings_nyc$price)) %>%
-        
-        addLegend(
-          "bottomright", pal = pal, values = ~review_scores_rating,
-          title = "Listings",
-          opacity = 0.7,
-          labels = labels
-        ) }
-    if (input$city == "Los Angeles") {
-      pal <- colorBin(c("#d7191c", "#fdae61", "#ffffbf", "#a6d96a", "#1a9641"), domain = listings_la$review_scores_rating, bins = bins)
-      map2 <- leaflet(listings_la) %>%
-        addProviderTiles("CartoDB.Positron", options = providerTileOptions(noWrap = TRUE)) %>%
-        addCircleMarkers(
-          lng = ~longitude,
-          lat = ~latitude,
-          radius=~price,
-          stroke=FALSE,
-          fillColor = ~pal(review_scores_rating),
-          color = "white",
-          fillOpacity=0.5,
-          popup = paste0( '<p><strong>', listings_la$name, '</strong> <p/>', 
-                          "<strong> Room Type: </strong>", 
-                          listings_la$room_type, '<br/>',
-                          "<strong> Price Per Night </strong>", 
-                          listings_la$price)) %>%
-        
-        addLegend(
-          "bottomright", pal = pal, values = ~review_scores_rating,
-          title = "Listings",
-          opacity = 0.7,
-          labels = labels
-        ) }
-  })
-  
+  # histogram of avg price graph 
   output$avgprice_graph <- renderPlot ({
-    ggplot(selected_city_prices(), aes(x = price, fill = room_type)) +
+    ggplot(selected_attributes(), aes(x = price, fill = room_type)) +
       geom_histogram(binwidth = 50) +
       labs(x = "average price", y = "count", title = "Airbnb Average Prices") +
       theme(legend.title = element_blank())
   })
+  
+  # parallel coordinate plot of avg ratings
+  output$avgratings_graph <- renderPlot ({
+    ggparcoord(selected_neighborhood_avg_ratings(), 
+               columns = 2:4, groupColumn = 1, scale = "globalminmax", title = "Average Ratings by Neighborhood")
+  })
+  
+  
+  #leafelt map
+  colorpal <- reactive({
+    req(input$input$room_types)
+    req(input$city)
+    if (input$city == "New York City") {
+      colorFactor("RdYlBu", listings_nyc$room_type) 
+    } else {
+      colorNumeric("RdYlBu", listings_la$room_type)
+    }
+  }) #So color will show type of room 
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron", 
+                       options = providerTileOptions(noWrap = TRUE)
+      )  %>%
+      setView(lng = -73.98928, lat = 40.75042, zoom = 10) #NYC coordinates just for now 
+  })
+  
+  observe({
+    pal <- colorpal()
+    
+    leafletProxy("mymap", data = selected_attributes()) %>%
+      clearShapes() %>%
+      addCircles(
+        lng = ~longitude,
+        lat = ~latitude,
+        color = "white",
+        fillColor = ~pal(room_type),
+        fillOpacity = 0.7, 
+        popup=~paste("<b>", name, "</b><br/>",
+       "Price: ", as.character(price), "<br/>"
+      )
+      )
+  })
+  
+  
   
 }
 
